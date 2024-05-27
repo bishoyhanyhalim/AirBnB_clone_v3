c9f17454334678be7b31a77263da6068a1b9c634
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-05-27 13:28:29 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """this is app file and main file"""
 
-from flask import Flask
-from models import storage
+from flask import Flask, jsonify
 from os import getenv
+from models import storage
 from api.v1.views import app_views
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,6 +14,13 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def call_storage_close(obj):
     storage.close()
+
+
+@app.errorhandler(404)
+def error_page(error):
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 
 if __name__ == "__main__":
